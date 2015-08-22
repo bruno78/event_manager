e4e9ce8c5da75819ee8dd0a4de6b5e36b5914133
Bruno G. Tavares <devbruno78@gmail.com>
2015-08-22 00:56:42 -0400

    Fix zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,14 @@
 require "csv"
 
+def zipcode_fix(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+
 puts "EventManager Initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
 	name = row[:first_name]
-	zipcode = row[:zipcode]
+	zipcode = zipcode_fix(row[:zipcode])
 	puts "#{name} #{zipcode}"
-end 
+end
