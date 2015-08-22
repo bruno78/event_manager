70ba57a099dd9f361ee8a0adf7846e0459608239
Bruno G. Tavares <devbruno78@gmail.com>
2015-08-21 23:05:20 -0400

    Display columns

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,7 @@
 puts "EventManager Initialized!"
 
-contents = File.read "event_attendees.csv"
-
-puts contents
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+	columns = line.split(',')
+	puts columns
+end
